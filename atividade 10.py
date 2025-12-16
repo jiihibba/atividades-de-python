d0f967880747e7e8ca970c8725a8f843f5dd2beb
jiihibba <erikisaac460@gmail.com>
2025-12-16 16:21:34 -0300

    Add files via upload

---
+++ atividade 10.py
@@ -0,0 +1,3 @@
+r  =float (input('quanto de dinheiro vc tem? :'))
+d = r/5.42
+print('com isso vc consegue tanto dolar{}'.format(d))
---
+++ atividade 8.py
@@ -0,0 +1,4 @@
+m = float(input('diga me uma distancia em metros: '))
+km = m / 1000
+print('a media de {}m corresponde a {}km'.format(m,km))
+
